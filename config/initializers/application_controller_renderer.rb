24fe017c49934b8e241c40f9203561e0a2a985a2
ryuhei shibata <shibataryuhei>
2019-12-18 13:55:15 +0900

    [start]5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8d48afb158da7a067f3e58ae775cade8dca0ac97112d7198acc2520f788e12141933de8cc8aa495044871e2edb6674be315e89d5d00aec325dc50dceefc57c65'
